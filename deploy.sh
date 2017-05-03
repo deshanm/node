10f5626001798ab9a9868e69864f7d073ce9c165
deshatom <deshatom@gmail.com>
2017-05-03 16:46:04 +0530

    added path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 -#!/bin/sh
 ssh jenkins@ubuntu-2gb-nyc2-01 <<EOF
+ # Absolute path to this script, e.g. /home/user/bin/foo.sh
+ SCRIPT=$(readlink -f "$0")
+ # Absolute path this script is in, thus /home/user/bin
+ SCRIPTPATH=$(dirname "$SCRIPT")
+ echo $SCRIPTPATH
  cd /node
  git pull
  npm install
